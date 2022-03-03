139d7ae2088b859df51597e4a82d135ce16fda43
Tiago Oliveira <dev.tiago.oliveira@gmail.com>
2022-03-03 11:48:10 -0300

    feat: ensure SignUpController returns an error if no name is provided

--- src/Presentation/Controllers/SignUpController.php
+++ src/Presentation/Controllers/SignUpController.php
@@ -4,12 +4,15 @@ declare(strict_types=1);
 
 namespace Tiagoliveirati\CleanArchPhpMango\Presentation\Controllers;
 
+use Error;
+
 class SignUpController
 {
     public function handle(mixed $httpRequest): mixed
     {
         return (object) [
-            'statusCode' => 400
+            'statusCode' => 400,
+            'body' => new Error('Missing param: name')
         ];
     }
 }
